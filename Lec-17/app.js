3f2749fde63792ed54afd49ac22c2e018be7f142
Tirth02 <pateltirth0902@gmail.com>
2026-03-04 09:52:20 -0500

    implemented like feature on both end as well as created edit page and handle things for edit at backend as well

--- Lec-17/app.js
+++ Lec-17/app.js
@@ -84,6 +84,31 @@ app.post("/post",isLoggedIn,async (req,res) => {
     await user.save();
     res.redirect("/profile");
 })
+
+app.get("/like/:id",isLoggedIn,async (req,res) => {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    if(post.likes.indexOf(req.user.userid) == -1)
+    {
+        post.likes.push(req.user.userid);
+    }
+    else
+    {
+        post.likes.splice(post.likes.indexOf(req.user.userid),1);
+    }
+    await post.save();
+    res.redirect("/profile");
+})
+
+app.get("/edit/:id",isLoggedIn,async(req,res)=>{
+    let post = await postModel.findOne({_id:req.params.id}).populate("user");
+    res.render("edit",{post});
+})
+
+app.post("/update/:id",isLoggedIn,async(req,res)=>{
+    let post = await postModel.findOneAndUpdate({_id:req.params.id},{content:req.body.content}); 
+    res.redirect("/profile");
+})
+
 async function isLoggedIn(req,res,next){
     if(!req.cookies.token || req.cookies.token === "") res.redirect("/login");
     else{
